a5b3e92d14aaf7c98aa36b8dd675041eef11ef29
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-10-05 21:50:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  //This is the code for the right movement
+function moveDodgerRight() {
+    const rightPos = dodger.style.left.replace('px', '');
+    const right = parseInt(rightPos, 10);
+
+    dodger.style.left = `${right + 1}px`;
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+}) 
